132f36564095e835f09161206c4d9675bdf2dc4d
Thomas Madden <madde120@mail.chapman.edu>
2017-11-08 12:21:34 -0800

    minor changes

--- patientPage.php
+++ patientPage.php
@@ -13,14 +13,12 @@
 <head>
     <title>Patient Page</title>
     <link rel="stylesheet" type="text/css" href="css/patientPage.css" />
-    <script type="text/javascript">
-        function dragLine() {
-
-        }
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 <div id="content_container">
+
+    <!-- Side Bar -->
     <div id="side_bar">
         <p class="info">
 Jane Doe
@@ -35,8 +33,11 @@ Insurance No. 342212
 Diagnoses:
     Cardiomyopathy
     Asthma
+
         </p>
     </div>
+
+    <!-- Anything to the right of the sidebar -->
     <div id="content">
         <div id="top_bar">
             <p class="info">
@@ -46,6 +47,8 @@ BP:         Temp:       Height:     Weight:     BMI:
 120/80      98 degrees  5' 3"       53.1 kg     24.2
             </p>
         </div>
+
+        <!-- Documents - Dynamic Height -->
         <div id="documents">
             <p class="info">
 Documents
@@ -55,11 +58,55 @@ Document 2
 Document 3
             </p>
         </div>
-        <div id="draggable_line" onmousedown="dragLine()"></div>
+
+        <!-- Line Placeholder -->
+        <div id="draggable_line"></div>
+
+        <!-- Current Chart (Class represents which chart is used) - Dynamic Height -->
         <div id="current_chart" class="overtime">
-            Current Chart
+            <p class="info">
+Current Chart
+            </p>
+        </div>
+
+        <!-- Scroll Menu -->
+        <div id="scroll_menu">
+            <a>Vital Signs</a><a>Growth Chart</a><a>Labs</a><a>Microbiology</a><a>Pathology</a>
         </div>
     </div>
 </div>
 </body>
 </html>
+
+<!-- Script for Draggable Line -->
+<script type="text/javascript">
+
+    var currentMousePos = { x: -1, y: -1 };
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+        currentMousePos.click = event.click;
+    });
+
+    $line = $("#draggable_line");
+    $docs = $("#documents");
+    $charts = $("#current_chart");
+
+    var finished = false;
+
+    $line.mouseup(function() {
+        finished = true;
+    });
+
+    $line.mousedown(function resize() {
+        alert("hi");
+        $docs.height(currentMousePos.y - $docs.offsetY);
+        $charts.height($charts.offsetY - currentMousePos.y);
+
+        if (finished === false) {
+            setTimeout(resize, 100)
+        }
+
+    });
+
+</script>
