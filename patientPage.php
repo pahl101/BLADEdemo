825af4147550817bd4fd00cb3bc3f94b939eadd3
pahl101 <pahl101@mail.chapman.edu>
2017-11-10 11:40:45 -0800

    updates to patient page, including partial working text editor

--- patientPage.php
+++ patientPage.php
@@ -11,16 +11,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+
     <title>Patient Page</title>
     <link rel="stylesheet" type="text/css" href="css/patientPage.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript">
+        window.addEventListener("load", function(){
+            var editor = the theWYSIWYG.document;
+            editor.designMode = "on";
+
+        },false);
+    </script>
 </head>
 <body>
+    <img style="margin-left: 5px" src="images/choc-logo-2014.png" alt="CHOC">
+
+    <ul class="nav nav-tabs w3-margin-bottom w3-margin-top">
+    <li><a href="index.php">Patient Menu</a></li>
+    <li class="active"><a href="#home">Current Patient<img src=images/x.png style="margin-left: 20px" onclick="location.href='index.php';"></a></li>
+  </ul>
 <div id="content_container">
 
     <!-- Side Bar -->
     <div id="side_bar">
         <p class="info">
+
+            <img src="images/user.jpg" height="150" width="150">
 Jane Doe
 
 Age: 14
@@ -34,6 +53,10 @@ Diagnoses:
     Cardiomyopathy
     Asthma
 
+Allergies:
+    Nut
+    Latex
+
         </p>
     </div>
 
@@ -44,13 +67,56 @@ Diagnoses:
 Today's Visit:
 
 BP:         Temp:       Height:     Weight:     BMI:
-120/80      98 degrees  5' 3"       53.1 kg     24.2
+120/80      98°          5' 3"       53.1 kg     24.2
             </p>
         </div>
 
         <!-- Documents - Dynamic Height -->
         <div id="documents">
+          <div id="theRibbon">
+            <button id="boldButton" title="Bold"><b>B</b></button>
+            <button id="italicButton" title="Italic"><em>I</em></button>
+            <button id="supButton" title="Superscript">X<sup>2</sup></button>
+            <button id="subButton" title="Subscript">X<sub>2</sub></button>
+            <button id="strikeButton" title="Strikethrough"><s>abc</s></button>
+            <button id="orderedListButton" title="Numbered list">(i)</button>
+            <button id="unorderedListButton" title="Bulleted list">&bull;</button>
+            <input type="color" id="fontColorButton" title="Change Font Color">
+            <input type="color" id="highlightButton" title="Highlight Text">
+            <select id= "fontChanger">
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Consolas">Consolas</option>
+                <option value="Tahoma">Tahoma</option>
+                <option value="monospace">Monospace</option>
+                <option value="cursive">Cursive</option>
+                <option value="sans-serif">Sans-Serif</option>
+                <option value="calibri">Calibri</option>
+            </select>
+            <script type="text/javascript">
+                var fonts = document.querySelectAll("select#fontChanger > option");
+                for (var i = 0; i < fonts.length; i++) {
+                    fonts[i].style.fontFamily = fonts[i].value;
+                }
+            </script>
+            <select id="fontSizeChanger">
+                <script type="text/javascript">
+                    for (var i=1; i<10; ++i){
+                        document.write("<option value='"+i+"'>"+i+"</option>");
+                    }
+                </script>
+            </select>
+            <button id="linkButton" title="Create Link">Link</button>
+            <button id="unlinkButton" title="Remove Link">UnLink</button>
+            <button id="undoButton" title="Undo">&larr;</button>
+            <button id="redoButton" title="Redo">&rarr;</button>
+          </div>
+          <div id="richTextArea">
+            <iframe id="theWYSIWYG" name="theWYSIWYG" frameborder="0"></iframe>
+          </div>  
             <p class="info">
+
+
+
 Documents
 
 Document 1
@@ -109,4 +175,6 @@ Current Chart
 
     });
 
+
 </script>
+
